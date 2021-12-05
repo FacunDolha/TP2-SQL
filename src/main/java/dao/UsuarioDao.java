c00b992c0af2b26060f03408081b6f675949d192
Alejandro Dolhagarats <ale_facu_mellis@hotmail.com>
2021-12-05 18:35:12 -0300

    Cambios en el Dao

--- src/main/java/dao/UsuarioDao.java
+++ src/main/java/dao/UsuarioDao.java
@@ -5,73 +5,109 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import persistence.commons.ConnectionProvider;
+import persistence.commons.GenericDao;
+import persistence.commons.MissingDataException;
 import turismoTierraMedia.TipoAtraccion;
 import model.Usuario;
+import model.nullobjects.NullUsuario;
 
 public class UsuarioDao implements GenericDao<Usuario> {
-	public Usuario findByName(String nombre) throws SQLException {
-		Usuario usuario = null;
-		Connection connection = ConnectionProvider.getConnection();
-		String query = "SELECT * " + "FROM usuarios "
-				+ "JOIN tipo_atraccion ON tipo_atraccion.id_tipo = usuarios.preferencia" + "WHERE nombre LIKE ?";
-		PreparedStatement statement = connection.prepareStatement(query);
-		statement.setString(1, nombre);
-		ResultSet resultSet = statement.executeQuery();
-		if (resultSet.next()) {
-			usuario = toUsuario(resultSet);
+
+	public Usuario findByUserame(String username) {
+		try {
+
+			String query = "SELECT * FROM usuarios WHERE username = ?";
+			Connection connection = ConnectionProvider.getConnection();
+			PreparedStatement statement = connection.prepareStatement(query);
+			statement.setString(1, username);
+			ResultSet resultSet = statement.executeQuery();
+
+			Usuario user = NullUsuario.build();
+			if (resultSet.next()) {
+				user = toUsuario(resultSet);
+			}
+			return user;
+		} catch (Exception e) {
+			throw new MissingDataException(e);
 		}
-		return usuario;
+
 	}
 
-	public List<Usuario> findAll() throws SQLException {
-		List<Usuario> usuarios = new ArrayList<Usuario>();
-		Connection connection = ConnectionProvider.getConnection();
-		String query = "SELECT * " + "FROM usuarios "
-				+ "JOIN tipo_atraccion ON tipo_atraccion.id_tipo = usuarios.preferencia";
-		PreparedStatement preparedStatement = connection.prepareStatement(query);
-		ResultSet resultSet = preparedStatement.executeQuery();
-		while (resultSet.next()) {
-			Usuario usuario = toUsuario(resultSet);
-			usuarios.add(usuario);
+	public List<Usuario> findAll() {
+		try {
+			String sql = "SELECT * FROM usuarios";
+			Connection conn = ConnectionProvider.getConnection();
+			PreparedStatement statement = conn.prepareStatement(sql);
+			ResultSet resultados = statement.executeQuery();
+
+			List<Usuario> usuarios = new LinkedList<Usuario>();
+			while (resultados.next()) {
+				usuarios.add(toUsuario(resultados));
+			}
+
+			return usuarios;
+		} catch (Exception e) {
+			throw new MissingDataException(e);
 		}
-		return usuarios;
 	}
 
-	private Usuario toUsuario(ResultSet resultSet) throws SQLException {
-		Integer idUsuario = resultSet.getInt("id_usuario");
-		String nombre = resultSet.getString("nombre");
-		Double presupuesto = resultSet.getDouble("monedas");
-		Double tiempoDisponible = resultSet.getDouble("tiempo");
-		// String tipoAtraccion = resultSet.getString("atraccion_preferida");
-		TipoAtraccion tipoPreferido = TipoAtraccion.valueOf(resultSet.getString("preferencia"));
-		Usuario usuario = new Usuario(idUsuario, nombre, presupuesto, tiempoDisponible, tipoPreferido);
-		// Habria que cambiar el constructor de Usuario y ver los nombres de las tablas
-		// de SQL de usuario
-		return usuario;
+	private Usuario toUsuario(ResultSet userRegister) throws SQLException {
+		return new Usuario(userRegister.getInt(1), userRegister.getString(2), userRegister.getString(3),
+				userRegister.getInt(5), userRegister.getDouble(6), userRegister.getBoolean(4));
 	}
 
+	/*
+	 * private Usuario toUsuario(ResultSet resultSet) throws SQLException { Integer
+	 * idUsuario = resultSet.getInt("id_usuario"); String nombre =
+	 * resultSet.getString("nombre"); Double presupuesto =
+	 * resultSet.getDouble("monedas"); Double tiempoDisponible =
+	 * resultSet.getDouble("tiempo"); // String tipoAtraccion =
+	 * resultSet.getString("atraccion_preferida"); TipoAtraccion tipoPreferido =
+	 * TipoAtraccion.valueOf(resultSet.getString("preferencia")); Usuario usuario =
+	 * new Usuario(idUsuario, nombre, presupuesto, tiempoDisponible, tipoPreferido);
+	 * // Habria que cambiar el constructor de Usuario y ver los nombres de las
+	 * tablas // de SQL de usuario return usuario; }
+	 */
 	public int insert(Usuario usuario) throws SQLException {
-		String sqlQuery = "INSERT INTO usuarios (nombre,monedas,tiempo,preferencia) " + "VALUES (?,?,?,?)";
-		Connection connection = ConnectionProvider.getConnection();
-		PreparedStatement statement = connection.prepareStatement(sqlQuery);
-		statement.setString(1, usuario.getNombre());
-		statement.setDouble(2, usuario.getPresupuesto());
-		statement.setDouble(3, usuario.getTiempoDisponible());
-		// statement.setString(4, usuario.getTipoPreferido().name());
-		statement.setInt(4, usuario.getTipoPreferido().ordinal() + 1);
-		// lo de ordinal lo pusimos para que de el orden de los enum. En las tabla en
-		// vez de poner text, seria un int.
-		int rowsUpdated = statement.executeUpdate();
-		return rowsUpdated;
+		try {
+
+			String sqlQuery = "INSERT INTO usuarios (username, password) VALUES (?, ?)";
+			Connection connection = ConnectionProvider.getConnection();
+			PreparedStatement statement = connection.prepareStatement(sqlQuery);
+			statement.setString(1, usuario.getUsername());
+			statement.setString(2, usuario.getPassword());
+			// statement.setDouble(3, usuario.getTiempoDisponible());
+			// statement.setString(4, usuario.getTipoPreferido().name());
+			// statement.setInt(4, usuario.getTipoPreferido().ordinal() + 1);
+			// lo de ordinal lo pusimos para que de el orden de los enum. En las tabla en
+			// vez de poner text, seria un int.
+			int rowsUpdated = statement.executeUpdate();
+			return rowsUpdated;
+		} catch (Exception e) {
+			throw new MissingDataException(e);
+		}
 	}
 
-	public int delete(Usuario usuario) throws SQLException {
-		return delete(usuario.getIdUsuario());
+	public int delete(Usuario usuario) {
+		try {
+			String sqlQuery = "DELETE FROM USERS WHERE USERNAME = ?";
+			Connection connection = ConnectionProvider.getConnection();
+
+			PreparedStatement statement = connection.prepareStatement(sqlQuery);
+			statement.setString(1, usuario.getUsername());
+			int rows = statement.executeUpdate();
+
+			return rows;
+		} catch (Exception e) {
+			throw new MissingDataException(e);
+		}
+
 	}
 
-	public int delete(Integer id) throws SQLException {
+	public int delete(Integer id) {
 		String sqlDeleteQuery = "DELETE FROM usuarios WHERE id=?";
 		Connection connection = ConnectionProvider.getConnection();
 		PreparedStatement statement = connection.prepareStatement(sqlDeleteQuery);
@@ -81,17 +117,21 @@ public class UsuarioDao implements GenericDao<Usuario> {
 	}
 
 	public int update(Usuario usuario) throws SQLException {
-		String sqlQuery = "UPDATE usuario" + "WHERE id = ?" + "SET nombre = ?, " + "presupuesto = ?, " + "tiempo = ?,"
-				+ "atraccion_preferida = ?";
-		Connection connection = ConnectionProvider.getConnection();
-		PreparedStatement statement = connection.prepareStatement(sqlQuery);
-		statement.setInt(1, usuario.getIdUsuario());
-		statement.setString(2, usuario.getNombre());
-		statement.setDouble(3, usuario.getPresupuesto());
-		statement.setDouble(4, usuario.getTiempoDisponible());
-		statement.setInt(4, usuario.getTipoPreferido().ordinal() + 1);
-		int rowsUpdate = statement.executeUpdate();
-		return rowsUpdate;
+		try {
+
+			String sqlQuery = "UPDATE usuario" + "WHERE id = ?" + "SET presupuesto = ?, " + "tiempo = ?";
+			Connection connection = ConnectionProvider.getConnection();
+			PreparedStatement statement = connection.prepareStatement(sqlQuery);
+			statement.setInt(1, usuario.getIdUsuario());
+			statement.setInt(2, usuario.getPresupuesto());
+			// statement.setDouble(3, usuario.getPresupuesto());
+			statement.setDouble(3, usuario.getTiempoDisponible());
+			// statement.setInt(4, usuario.getTipoPreferido().ordinal() + 1);
+			int rowsUpdate = statement.executeUpdate();
+			return rowsUpdate;
+		} catch (Exception e) {
+			throw new MissingDataException(e);
+		}
 	}
 
 	public int countAll(Usuario usuario) throws SQLException {
@@ -104,18 +144,51 @@ public class UsuarioDao implements GenericDao<Usuario> {
 		return total;
 	}
 
-	public int countAll() throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+	public int countAll() {
+		try {
+			String sql = "SELECT COUNT(1) AS TOTAL FROM USERS";
+			Connection conn = ConnectionProvider.getConnection();
+			PreparedStatement statement = conn.prepareStatement(sql);
+			ResultSet resultados = statement.executeQuery();
+
+			resultados.next();
+			int total = resultados.getInt("TOTAL");
+
+			return total;
+		} catch (Exception e) {
+			throw new MissingDataException(e);
+		}
 	}
 
-	public Usuario findById(Integer id) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+	public Usuario find(Integer id) {
+		try {
+			String sql = "SELECT * FROM USERS WHERE ID = ?";
+			Connection conn = ConnectionProvider.getConnection();
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setInt(1, id);
+			ResultSet resultados = statement.executeQuery();
+
+			Usuario user = NullUsuario.build();
+
+			if (resultados.next()) {
+				user = toUsuario(resultados);
+			}
+
+			return user;
+		} catch (Exception e) {
+			throw new MissingDataException(e);
+		}
+
 	}
 
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
 		return false;
+	}
+
+	@Override
+	public Usuario findById(Integer id) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
